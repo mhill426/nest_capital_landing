53cd0519c4dcea3a28eacd74a6a6b0d353aa7ef9
mhill426 <mhill426@gmail.com>
2022-03-21 08:03:27 -0400

    initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// function randomImage(){
+//   var images = [
+//    'https://download.unsplash.com/photo-1428278953961-a8bc45e05f72',
+//    'https://download.unsplash.com/photo-1639976518727-4c21281644e0',
+//    'https://download.unsplash.com/photo-1429516387459-9891b7b96c78'];
+//   var size = images.length;
+//   var x = Math.floor(size * Math.random());
+//   console.log(x);
+//   var element = document.getElementsByClassName('home-intro');
+//   console.log(element);
+//   element[0].style["background-image"] = "url("+ images[x] + ")";
+// }
+
+// document.addEventListener("DOMContentLoaded", randomImage);
---
+++ readme.md
@@ -0,0 +1,1 @@
+Temporary landing page for Nest Capital
